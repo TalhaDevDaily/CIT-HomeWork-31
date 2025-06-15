ccf17f5048c325311ee104a333e683703884c9e5
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-06-16 02:09:35 +0600

    Weather API

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const handleCheckWeather = function () {
+  console.log(`Hello World from World`);
+};
+
+console.log(`1`);
+
+console.log(`2`);
+
+try {
+  console.lo(`3`);
+} catch (error) {
+  console.log(error);
+}
+
+console.log(`4`);
+
+console.log(`5`);

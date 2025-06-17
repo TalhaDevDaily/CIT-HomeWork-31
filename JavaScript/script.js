ea82c115a3825117da937faba40a36ac1bc5c80b
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-06-17 18:25:55 +0600

    API 20% done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,16 +4,16 @@ const handleCheckWeather = function () {
   console.log(`Hello World from World`);
 };
 
-console.log(`1`);
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?lat=23.8103&lon=90.4125&appid=0133a01997a0515f964051513c4d9a2c`
+)
+  .then((res) => res.json())
+  .then((weather) => {
+    console.log(weather);
+  })
+  .catch((errorMsg) => {
+    console.log(errorMsg);
+  });
 
-console.log(`2`);
-
-try {
-  console.lo(`3`);
-} catch (error) {
-  console.log(error);
-}
-
-console.log(`4`);
-
-console.log(`5`);
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// Weather API Key:
